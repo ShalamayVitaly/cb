92373fe47966a4c4933b46be8462b966fee8cf4d
Vitaly Shalamay <vitalyshalamay@gmail.com>
2022-02-21 22:35:16 +0200

    AdditionCalculatorApp

--- projects/AdditionCalculatorApp/AdditionCalculatorApp/Program.cs
+++ projects/AdditionCalculatorApp/AdditionCalculatorApp/Program.cs
@@ -12,7 +12,8 @@ namespace AdditionCalculatorApp
         {
             int a = 2;
             int b = 4;
-            int c = a + b; // 2 + 4
-        }
+
+            Console.WriteLine("a+b");
+         }
     }
 }
